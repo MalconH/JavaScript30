5262381d3719d016807404391068928575f2e726
Malcon <malcon.heltner@gmail.com>
2024-03-04 10:15:33 -0300

    Add solution for Type ahead

--- 06 - Type Ahead/index.js
+++ 06 - Type Ahead/index.js
@@ -1,17 +1,27 @@
 const cities = [];
 
 function populateCities() {
-  const endpoint =
-    "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+  const endpoint = "https://apis.datos.gob.ar/georef/api/localidades?max=5000";
 
   // If it doesn't exist in local storage
   if (!localStorage.getItem("cities")) {
     fetch(endpoint)
       .then((response) => response.json())
       .then((data) => {
-        cities.push(...data);
+        // Extracting cities (localidades) and provinces (provincias)
+        const mappedData = data.localidades.map((infoLocalidad) => {
+          const {
+            nombre: city,
+            provincia: { nombre: province },
+          } = infoLocalidad;
+
+          return { city, province };
+        });
+
+        cities.push(...mappedData);
+
         // save to local storage
-        localStorage.setItem("cities", JSON.stringify(cities));
+        localStorage.setItem("cities", JSON.stringify(mappedData));
       });
   } else {
     // retrieve from local storage
@@ -23,8 +33,8 @@ function populateCities() {
 function matchCities(wordToMatch, cities) {
   const cityRegex = new RegExp(wordToMatch, "i");
   const matches = cities.filter((cityObj) => {
-    // check for coincidence in either city name or state name
-    return cityRegex.test(cityObj.city) || cityRegex.test(cityObj.state);
+    // check for coincidence in either city name or province name
+    return cityRegex.test(cityObj.city) || cityRegex.test(cityObj.province);
   });
 
   return matches;
@@ -33,16 +43,16 @@ function matchCities(wordToMatch, cities) {
 function showDefaultSuggestions() {
   const $suggestions = document.querySelector(".suggestions");
   $suggestions.innerHTML = `
-    <li>Filter for a city</li>
-    <li>or a state</li>`;
+    <li>Filtrá por ciudad</li>
+    <li>o provincia</li>`;
 }
 
 function formatNumber(number) {
   return new Intl.NumberFormat().format(number);
 }
 
-function showMatches() {
-  const userInput = this.value; // this => input.search
+function showMatches(max = 25) {
+  const userInput = document.querySelector(".search").value;
 
   // Without this, it will match all the cities
   if (!userInput) {
@@ -51,29 +61,44 @@ function showMatches() {
   }
   const $suggestions = document.querySelector(".suggestions");
 
-  const places = matchCities(userInput, cities);
-
-  const html = places
+  const matches = matchCities(userInput, cities);
+  const html = matches
+    .slice(0, max) // shows first 25 results by default
     .map((place) => {
       // Create a regex that will search for the input.value
       const placeRegex = new RegExp(userInput, "gi");
 
       // Replace every occurrence with a <span> containing the value the input.value
       const cityName = place.city.replace(placeRegex, `<span class="hl">${userInput}</span>`);
-      const stateName = place.state.replace(placeRegex, `<span class="hl">${userInput}</span>`);
-      const population = formatNumber(place.population);
+      const provinceName = place.province.replace(
+        placeRegex,
+        `<span class="hl">${userInput}</span>`
+      );
 
       return `
       <li>
-        <span>${cityName}, ${stateName}</span>
-        <span class="population">${population}</span>
+        <span>${cityName}, ${provinceName}</span>
       </li>`;
     }) // -> returns an array
     .join(""); // make it a string;
 
   $suggestions.innerHTML = html;
+
+  // Generate "show more" button
+  if (max < matches.length) {
+    const $showMore = document.createElement("li");
+    $showMore.textContent = "Mostrar más...";
+    $showMore.className = "show-more";
+    $showMore.addEventListener("click", () => {
+      showMatches(1000);
+    });
+
+    $suggestions.appendChild($showMore);
+  }
 }
 
-document.querySelector(".search").addEventListener("input", showMatches);
+document.querySelector(".search").addEventListener("input", () => {
+  showMatches();
+});
 
 populateCities();
