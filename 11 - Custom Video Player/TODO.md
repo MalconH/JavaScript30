7ad0980f3eb6a151cf5139e896230d231b848c17
Malcon <malcon.heltner@gmail.com>
2024-04-03 10:18:05 -0300

    this is before fixing progressbar update moving mouse

---
+++ 11 - Custom Video Player/TODO.md
@@ -0,0 +1,14 @@
+# TODO
+
+[X] play pausa
+[X] selector volumen
+[X] selecor velocidad
+[X] que se mueva la barrita segun el progreso video
+THIS: [ ] selector segundos video
+  Lo facil es agarrar la x donde se hizo click y ponerselo
+  El tema es que la watchedBar toma un porcentaje
+
+[ ] adelantar y atrasar
+[ ] playpausa al clicar video
+
+Problema de la barra de progreso: cuando hago click le paso los px relativos a la barra de progreso al currentTime, no esos px traducidos a tiempo
---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,71 @@
+let isPlaying = false;
+let isHoldingClick = false;
+
+const $video = document.querySelector("video.viewer");
+const $volume = document.querySelector("#volume");
+const $playbackRate = document.querySelector("#playbackRate");
+const $playButton = document.querySelector(".toggle");
+const $watchedBar = document.querySelector(".progress__filled");
+const $progressBar = document.querySelector(".progress");
+
+$video.addEventListener("timeupdate", handleTimeUpdate);
+$volume.addEventListener("input", changeVolume);
+$playbackRate.addEventListener("input", changePlaybackRate);
+$playButton.addEventListener("click", togglePlay);
+$progressBar.addEventListener("mousedown", (e) => {
+  isHoldingClick = true;
+  changeProgress(e);
+});
+$progressBar.addEventListener("mousemove", changeProgress);
+window.addEventListener("mouseup", () => {
+  isHoldingClick = false;
+});
+$progressBar.addEventListener("mouseleave", () => {
+  isHoldingClick = false;
+});
+
+function changeProgress(e) {
+  if (!isHoldingClick) return;
+
+  // fullWidth - video.duration
+  // px - x
+  // x = px * duracionTotal / fullwidth
+  const progress = (e.offsetX * $video.duration) / $progressBar.offsetWidth;
+  console.log(progress);
+  $video.currentTime = progress;
+}
+
+function togglePlay(e) {
+  // Play -> pause and viceversa
+  isPlaying = !isPlaying;
+
+  if (isPlaying) {
+    $video.play();
+    e.target.textContent = "❚❚";
+  }
+
+  if (!isPlaying) {
+    $video.pause();
+    e.target.textContent = "▶";
+  }
+}
+
+function changeVolume(e) {
+  const volume = Number(e.target.value);
+  $video.volume = volume;
+}
+
+function changePlaybackRate(e) {
+  const playbackRate = Number(e.target.value);
+  $video.playbackRate = playbackRate;
+}
+
+function handleTimeUpdate() {
+  // Obtain the % of video reproduced
+  // const progressPercentage = ($video.currentTime / $video.duration) * 100 || 0;
+  const secondsToPx = $progressBar.offsetWidth / $video.duration;
+  const progress = $video.currentTime * secondsToPx;
+  // Applying it to progressBar
+  // $watchedBar.style.flexBasis = `${progressPercentage}%`;
+  $watchedBar.style.flexBasis = `${progress}px`;
+}
